ed69b660fc8fea9af8f9d17102f01992b5844f80
nabila kayla rafa <nabilakayla.005@gmail.com>
2026-02-19 17:22:01 +0700

    Titik Pertemuan 1

---
+++ Pertemuan1/src/MTitik.java
@@ -0,0 +1,15 @@
+/* Nama File    : MTitik.java 
+ * Deskripsi    : berisi atribut dan method dalam class Titik
+ * Pembuat      : Nabila Kayla Rafa (24060124120022)
+ * Tanggal      : Kamis, 19 Februari 2026 */
+
+public class MTitik {
+    public static void main(String[] args) {
+        Titik T1 = new Titik(); //Membuat objek titik T1 (0,0)
+        T1.setAbsis(3);
+        T1.setOrdinat(4);
+        T1.printTitik();
+        T1.geser(3, 4);
+        T1.printTitik();
+    }
+}
